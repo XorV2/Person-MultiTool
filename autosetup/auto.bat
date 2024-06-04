5d5918df8e6f630299347ae9ab268fa377a662f9
Rooben <75034784+XorV2@users.noreply.github.com>
2024-06-04 17:53:42 +0100

    Create auto.bat

---
+++ autosetup/auto.bat
@@ -0,0 +1,6 @@
+echo "Downloading python modules, make sure you're using python 3.9 or later and have pip installed"
+echo "This will just run a few pip installs, shouldn't take too long"
+
+pip install colorama
+pip install websocket
+pip install tls_client
